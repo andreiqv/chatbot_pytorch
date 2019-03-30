636b8edcf0d1c9c2ed67994e756e8465c9cf1ece
z <phxv@mail.ru>
2019-03-31 01:34:46 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+import os
+######### 
+data_dir = '../data'
+corpus_name = 'corpus'
+#corpus_name = "corpus"
+corpus = os.path.join(data_dir, corpus_name)
+# Define path to new file
+datafile = os.path.join(corpus, "formatted_movie_lines.txt")
+
+MAX_LENGTH = 10  # Maximum sentence length to consider
